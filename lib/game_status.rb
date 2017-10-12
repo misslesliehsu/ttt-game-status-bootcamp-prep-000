dd2d49a4c3630986fea45054d4777a03ec995f6a
Leslie Hsu <chuckleberries@hotmail.com>
2017-10-12 15:01:26 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -3,4 +3,14 @@ def position_taken?(board, index)
   !(board[index].nil? || board[index] == " ")
 end
 
+WIN_COMBINATIONS = [
+  [0,1,2],
+  [0,3,6],
+  [0,4,8],
+  [1,4,7],
+  [2,5,8],
+  [3,4,5],
+  [6,7,8],
+  [6,4,2]
+]
 # Define your WIN_COMBINATIONS constant
