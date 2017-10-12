4115ce8d03e7b0940f98302bdd8639cd17b36b5a
Leslie Hsu <chuckleberries@hotmail.com>
2017-10-12 15:50:38 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -15,13 +15,12 @@ WIN_COMBINATIONS = [
 ]
 
 def won?(board)
-  WIN_COMBINATIONS.each do |winning_triplet|
+  WIN_COMBINATIONS.detect do |winning_triplet|
     if winning_triplet[0] =="X" && winning_triplet[1] == "X" && winning_triplet[2] == "X"
       return winning_triplet
       break
     elsif winning_triplet[0] == "O" && winning_triplet[1] == "O" && winning_triplet[2] == "O"
       return winning_triplet
-      break
     else
       return nil
     end
